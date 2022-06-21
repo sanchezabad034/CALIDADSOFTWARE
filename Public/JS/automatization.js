1f3ff694201508093da1faa3f7b85d7e2e114317
sanchezabad034 <68512055+sanchezabad034@users.noreply.github.com>
2022-06-21 14:10:11 -0500

    Cambios en automatization.js

--- Public/JS/automatization.js
+++ Public/JS/automatization.js
@@ -1,22 +1,21 @@
-// let slider = document.querySelector(".container");
-// let slidertext= document.querySelectorAll(".slider__text");
-// let img= document.querySelectorAll(".slider__img");
+//  let slider = document.querySelector(".container");
+ let slidertext= document.querySelectorAll(".slider__text");
+ let img= document.querySelectorAll(".slider__img");
 
-// let contador =1;
-// let width = slidertext[0].clientWidth;
+let contador =1;
+let width = slidertext[0].clientWidth;
 
-// let intervalo= 3000;
+ let intervalo= 3000;
 
-// window.addEventListener("resize", function(){
-//      width = slidertext[0].clientWidth;
-// });
+ window.addEventListener("resize", function(){
+      width = slidertext[0].clientWidth;
+ });
+ setInterval(function(){
+      slides();
+ }, intervalo);
 
-// setInterval(function(){
-//      slides();
-// }, intervalo);
-
-// function slides(){
-//      slider.style.transform="translate("+(-width*contador)+"px)";
-//      slider.style.transition="transform 3s";
-//      contador++;
-// }
+ function slides(){
+           slider.style.transform="translate("+(-width*contador)+"px)";
+   slider.style.transition="transform 3s";
+      contador++;
+ }

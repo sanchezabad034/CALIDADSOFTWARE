8d3519e080a9eda3b142008bce2898bc7c6f1f34
sanchezabad034 <68512055+sanchezabad034@users.noreply.github.com>
2022-06-21 18:54:21 -0500

    CS04-Cambios pg.js se agrego una funcionalidad para validar datos del formulario de contacto

--- Public/JS/pg.js
+++ Public/JS/pg.js
@@ -1,6 +1,6 @@
- let formname=document.querySelector("#form_name");
- let email = document.querySelector("#form_email");
- let message= document.querySelector("#message_form");
+ //let formname=document.querySelector("#form_name");
+ //let email = document.querySelector("#form_email");
+ //let message= document.querySelector("#message_form");
 
 if(formname==""){console.log("Ingresa el campo texto!");}
 else if (email == ""){console.log("Ingresa un email!");}

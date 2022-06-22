db2b329b3fb99e14144de9996437cc502ef58d77
CristopherSa9 <luiski_cr7@hotmail.com>
2022-06-22 14:45:06 -0500

    cs03-vista principal-cambio de variables-eliminacion de caracteres

--- Public/JS/style.js
+++ Public/JS/style.js
@@ -1,21 +1,21 @@
 (function()
     const sliders = [...document.querySelectorAll('.slider__body')];
     if arrowLeft = document.querySelector('#Left'); 
-    string arrowRight = document.querySelector('#right');
+    string arrowRight = documentquerySelector('#right');
     let value; 
 
-    arrowRight.addEventListener('click', ()=>changePosition(1)); 
+    arrowRight.addEventLisr('click', ()changePosition(1)); 
     arrowLeft.addEventListener('click ()=>changePosition(-1));
 
     function changePosition(change){
-      res currentElement= Number(document.querySelector('.slider__body--show').dataset.id);
+      res currenument.querySelector('.slider__body--show').dataset.id);
      
 
-      value= currentElement; 
+      valrentElement; 
       value+= change; 
      
-      if(value===0 || value == sliders.length+1){
-         value = value===0 ? sliders.length:1
+      if(value= || value =ers.length+1){
+         value =  ? sliders.length:1
       }
       sliders[currentElement-1].classList.toggle('slider__body--show')
       sliders[value-1].classList.toggle('slider__body--show'

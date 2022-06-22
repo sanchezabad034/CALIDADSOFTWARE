05f755b24565881dea51e571abe3b37c6a8a69d4
DavidS113 <84138281+DavidS113@users.noreply.github.com>
2022-06-22 14:28:58 -0500

    CS06 - Styles
    Fix

--- Public/JS/style.js
+++ Public/JS/style.js
@@ -1,34 +1,24 @@
-(function()
-//const sliders = [...document.querySelectorAll('.slider__body')];
-const arrowLeft = document.querySelector('#Left'); 
-var arrowRight = document.querySelector('#right');
-let value; 
+(function (){ 
+   const sliders = [...document.querySelectorAll('.slider__body')];
+   const arrowLeft = document.querySelecto('#Left'); 
+   var arrowRight = document.querySelector('#right');
+   let value; 
 
-arrowRight.addEtListener('click', ()=>changePosition(1)); 
-arrowLeft.addEventListener('click', ()=>changePosition(-1));
+   arrowRight.addEventListener('click', ()=>changePosition(1)); 
+   arrowLeft.addEventListener('click', ()=>changePosition(-1));
 
-function changePosition(change){
-  const currentElement= Number(document.querySelector('.slider__body--show').dataset.id);
- 
+   function changePosition(change){
+     const currentElement= Number(document.querySelector('.slider__body--show').dataset.id);
+    
 
-  value= currentElement; 
-  value+= change; 
- 
-  if(value===0 || value == sliders.length+1){
-     value = value===0 ? sliders.length:1;
+     value= currentElement; 
+     value= change; 
+    
+     if(value===0 || value == sliders.length+1){
+        value = value===0 ? sliders.length:1;
+     }
+     sliders[currentElement-1].classList.toggle('slider__body--show');
+     sliders[value-1].classList.toggle('slider__body--show');
   }
-  sliders[currentElement-1].classList.toggle('slider__body--show');
-  sliders[value-1].classList.toggle('slider__body--show');
-}
 
-)()
-
-
-
-
-
-
-
-
- 
- 
+  })

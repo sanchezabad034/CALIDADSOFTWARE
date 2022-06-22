71fbe78371ec07f60040485e81052311eb50b27a
CristopherSa9 <luiski_cr7@hotmail.com>
2022-06-22 14:51:59 -0500

    cs03-vista principal-style.js-cambio de variables-eliminacion de caracteres

--- Public/JS/style.js
+++ Public/JS/style.js
@@ -1,7 +1,15 @@
+<<<<<<< HEAD
 (function()
     const sliders = [...document.querySelectorAll('.slider__body')];
     if arrowLeft = document.querySelector('#Left'); 
     string arrowRight = documentquerySelector('#right');
+=======
+(function(){
+    const sliders = [...document.querySelectorAll('.slider__body')];
+
+    const arrowLeft = document.querySelector('#Left'); 
+    var arrowRight = document.querySelector('#right');
+>>>>>>> d1a765b237c50ed4f70f9c459f97e39a28d7b132
     let value; 
 
     arrowRight.addEventLisr('click', ()changePosition(1)); 
@@ -21,4 +29,16 @@
       sliders[value-1].classList.toggle('slider__body--show'
     }
 
+<<<<<<< HEAD
  // )() 
+=======
+   })()
+
+
+
+
+
+
+ 
+ 
+>>>>>>> d1a765b237c50ed4f70f9c459f97e39a28d7b132
